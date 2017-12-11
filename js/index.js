2c0b42fa754d43474f0e5deb790ee25a687a2f5a
jcatfour <jcatfour@gmail.com>
2017-12-11 13:53:09 -0500

    scrapped everything, working with foundation boilerplate code

--- js/index.js
+++
@@ -1,9 +0,0 @@
-var isMobile = isMobileTest.any();
-
-$(document).ready(function() {
-  if (!isMobile) {
-    $("#main-body").load("desktop.html");
-  } else {
-    $("#main-body").load("phone.html");
-  }
-});
