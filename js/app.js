fa88bf0a0d3f0ca59370a303dd11a3870f9cdb9c
jcatfour <jcatfour@gmail.com>
2017-12-13 17:53:11 -0500

    icons (not banner) fade in when loaded

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 $(document).foundation()
+
+$(document).ready(function() {
+  console.log("ehh");
+  //$(".pending-img-load").toggleClass("img-loaded");
+  $(".pending-img-load").toggleClass("pending-img-load");
+});
