16494b66c46203238de7dc61dfb33f335f97682c
jcatfour <jcatfour@gmail.com>
2017-12-13 21:02:49 -0500

    added contact div animation, WIP

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 $(document).foundation()
 
+var contactCollapsed = false;
+
 $(document).ready(function() {
-  console.log("ehh");
-  //$(".pending-img-load").toggleClass("img-loaded");
   $(".pending-img-load").toggleClass("pending-img-load");
+});
+
+$(".contact").on("click", function() {
+  if (window.matchMedia('(min-width: 600px)').matches) {
+    $(".contact-div").toggleClass("contact-collapsed");
+  } else {
+    alert("You on mobile, do thing");
+  }
 });
