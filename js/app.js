4cdce325f0fcfab84199ba08b1e77f08d7e15626
jcat4 <jcatfour@gmail.com>
2017-12-17 18:59:06 -0600

    scrapped everything again, startin with new design

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
 $(document).foundation()
-
-var contactCollapsed = false;
-
-$(document).ready(function() {
-  $(".pending-img-load").toggleClass("pending-img-load");
-});
-
-$(".contact").on("click", function() {
-  if (window.matchMedia('(min-width: 600px)').matches) {
-    $(".contact-div").toggleClass("contact-collapsed");
-    $(".inner-contact").toggleClass("hidden");
-  } else {
-    alert("You on mobile, do thing");
-  }
-});
