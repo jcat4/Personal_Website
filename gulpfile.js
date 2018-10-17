7ec707483c79cd0a03203e8ec97e854cdd92974b
jcat4 <jcatfour@gmail.com>
2018-10-16 21:34:24 -0500

    pretty much finished bundling/optimization, just gotta do font shit eventually

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp'),
       sass = require('gulp-sass'),
-      browserSync = require('browser-sync').create();
+      browserSync = require('browser-sync').create(),
+      useref = require('gulp-useref'),
+      uglify = require('gulp-uglify'),
+      gulpIf = require('gulp-if'),
+      cssnano = require('gulp-cssnano'),
+      imagemin = require('gulp-imagemin'),
+      cache = require('gulp-cache'),
+      del = require('del');
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss')
@@ -24,3 +31,22 @@ gulp.task('watch', ['browserSync', 'sass'], function () {
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
 });
+
+gulp.task('images', function(){
+    return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('build', ['sass', 'images'], function(){
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+})
