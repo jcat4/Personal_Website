e4d47a2559905a264535c52a29196a2469b0b864
jcatfour@gmail.com <joey.cardosi@gmail.com>
2018-10-17 12:54:59 -0500

    took away animation (for now), and redid responsive styling

--- app/js/app.js
+++ app/js/app.js
@@ -2,17 +2,17 @@ $(document).foundation();
 
 $(document).ready(function() {
   // If Desktop, initiate animations
-  if (window.matchMedia("(min-width: 600px)")) {
-    $(".portrait").css({
-      "-webkit-transform": "scale(1, 1)",
-      "-moz-transform": "scale(1, 1)",
-      "-ms-transform": "scale(1, 1)",
-      "-o-transform": "scale(1, 1)",
-      "transform": "scale(1, 1)"
-    });
-    $(".break").css("width", "18vw");
-    $(".head-text").css("padding-top", "0");
-    $(".temp-add-msg").css("opacity", "1");
-  }
+  // if (window.matchMedia("(min-width: 600px)")) {
+  //   $(".portrait").css({
+  //     "-webkit-transform": "scale(1, 1)",
+  //     "-moz-transform": "scale(1, 1)",
+  //     "-ms-transform": "scale(1, 1)",
+  //     "-o-transform": "scale(1, 1)",
+  //     "transform": "scale(1, 1)"
+  //   });
+  //   $(".break").css("width", "18vw");
+  //   $(".head-text").css("padding-top", "0");
+  //   $(".temp-add-msg").css("opacity", "1");
+  // }
 
 });
