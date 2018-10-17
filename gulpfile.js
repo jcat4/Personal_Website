bd396e8050eb3156ce4b0c989aebcda61d518236
jcat4 <jcatfour@gmail.com>
2018-10-16 20:38:02 -0500

    gulp watch stuff done, now to do bundling/optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 const gulp = require('gulp'),
-      sass = require('gulp-sass');
+      sass = require('gulp-sass'),
+      browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-    return gulp.src('app/scss/styles.scss')
+    return gulp.src('app/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    });
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
 });
