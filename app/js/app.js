9d53339b4119c6e2cbdc8ed1eb08ced5d9340016
jcat4 <jcatfour@gmail.com>
2018-12-21 15:37:48 -0600

    image changes based on month now!

--- app/js/app.js
+++ app/js/app.js
@@ -1,44 +1,33 @@
-// $(document).foundation();
-
 $(document).ready(function() {
-//   // If Desktop, initiate animations
-//   // if (window.matchMedia("(min-width: 600px)")) {
-//   //   $(".portrait").css({
-//   //     "-webkit-transform": "scale(1, 1)",
-//   //     "-moz-transform": "scale(1, 1)",
-//   //     "-ms-transform": "scale(1, 1)",
-//   //     "-o-transform": "scale(1, 1)",
-//   //     "transform": "scale(1, 1)"
-//   //   });
-//   //   $(".break").css("width", "18vw");
-//   //   $(".head-text").css("padding-top", "0");
-//   //   $(".temp-add-msg").css("opacity", "1");
-//   // }
 
-  // set colors
-  var className,
-    currMonth = (new Date()).getMonth() + 1,
-    particleJsConfig = null;
+  // set up conditional styling and components
+  var bgClass,
+      portraitCls,
+      currMonth = (new Date()).getMonth() + 1,
+      particleJsConfig = null;
 
   switch (currMonth) {
     case 12:
-      className = 'red-bg';
+      bgClass = 'red-bg';
       particleJsConfig = '/js/vendor/particle-configs/snow.json';
+      portraitCls = 'default-me'; // todo add santa version!
       break;
     default:
-      className = 'normal-bg';
+      bgClass = 'normal-bg';
+      portraitCls = 'default-me';
       break;
   }
 
   // will be set if particleJS is needed
   if (particleJsConfig) {
     particlesJS.load('particles-js', particleJsConfig, function() {
-      $('#particles-js').children().addClass(className);
+      $('#particles-js').children().addClass(bgClass);
       console.log('callback - particles.js config loaded');
     });
   } else {
-    $('.top-div').addClass(className);
+    $('.top-div').addClass(bgClass);
   }
-  
-  // $('.top-div').addClass('normal-bg');
+
+  // set that portrait image!
+  $(".portrait").addClass(portraitCls);
 });
